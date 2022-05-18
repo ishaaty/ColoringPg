4d2603cad6ab8b8d1664e8ae282962f6f015a47e
ishaaty <ishatyagi888@gmail.com>
2022-05-18 10:46:13 -0400

    updated js file

--- coloringPg.js
+++ coloringPg.js
@@ -1,17 +1,26 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-var redBtn = document.querySelector("#red");
-var orangeBtn = document.querySelector("#orange");
-var yellowBtn = document.querySelector("#yellow");
-var greenBtn = document.querySelector("#green");
-var blueBtn = document.querySelector("#blue");
-var purpleBtn = document.querySelector("#purple");
-var pinkBtn = document.querySelector("#pink");
-var whiteBtn = document.querySelector("#white");
-var blackBtn = document.querySelector("#black");
-var clearBtn = document.querySelector("#clear");
+// const redBtn = document.querySelector("#red") as HTMLButtonElement;
+// const orangeBtn = document.querySelector("#orange") as HTMLButtonElement;
+// const yellowBtn = document.querySelector("#yellow") as HTMLButtonElement;
+// const greenBtn = document.querySelector("#green") as HTMLButtonElement;
+// const blueBtn = document.querySelector("#blue") as HTMLButtonElement;
+// const purpleBtn = document.querySelector("#purple") as HTMLButtonElement;
+// const pinkBtn = document.querySelector("#pink") as HTMLButtonElement;
+// const whiteBtn = document.querySelector("#white") as HTMLButtonElement;
+// const blackBtn = document.querySelector("#black") as HTMLButtonElement;
+// const clearBtn = document.querySelector("#clear") as HTMLButtonElement;
 var pairsList = [];
 var color = "black";
+// document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
+//     color = elm.dataset.color;
+//   })
+document.querySelectorAll(".colorBtn").forEach(function (elm) {
+    elm.addEventListener("click", function (event) {
+        var clickedColor = event.currentTarget;
+        color = clickedColor.dataset.key;
+    });
+});
 for (var i = 0; i <= canvas.height; i += 40) {
     for (var j = 0; j <= canvas.width; j += 40) {
         pairsList.push([i, j]);
@@ -41,12 +50,12 @@ function makeGrid(startX, startY, x, y, inc) {
     }
 }
 makeGrid(0, 0, canvas.width, canvas.height, 40);
-color = "white";
 for (var _i = 0, pairsList_1 = pairsList; _i < pairsList_1.length; _i++) {
     var p = pairsList_1[_i];
+    color = "white";
     makeGrid(p[0] - 38, p[1] - 38, p[0] - 1, p[1] - 1, 1);
+    color = "black";
 }
-color = "black";
 canvas.addEventListener("click", function (event) {
     for (var _i = 0, pairsList_2 = pairsList; _i < pairsList_2.length; _i++) {
         var p = pairsList_2[_i];
@@ -55,40 +64,36 @@ canvas.addEventListener("click", function (event) {
         }
     }
 });
-redBtn.addEventListener("click", function (event) {
-    color = "red";
-});
-orangeBtn.addEventListener("click", function (event) {
-    color = "orange";
-});
-yellowBtn.addEventListener("click", function (event) {
-    color = "yellow";
-});
-greenBtn.addEventListener("click", function (event) {
-    color = "green";
-});
-blueBtn.addEventListener("click", function (event) {
-    color = "blue";
-});
-purpleBtn.addEventListener("click", function (event) {
-    color = "purple";
-});
-pinkBtn.addEventListener("click", function (event) {
-    color = "pink";
-});
-whiteBtn.addEventListener("click", function (event) {
-    color = "white";
-});
-blackBtn.addEventListener("click", function (event) {
-    color = "black";
-});
-clearBtn.addEventListener("click", function (event) {
-    color = "white";
-    for (var _i = 0, pairsList_3 = pairsList; _i < pairsList_3.length; _i++) {
-        var p = pairsList_3[_i];
-        makeGrid(p[0] - 38, p[1] - 38, p[0] - 1, p[1] - 1, 1);
-    }
-});
-// heredity : transmission of traits from one generation to next
-// character : heritable feature that varies among individuals
-// trait : variant for a character
+// redBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "red";
+// });
+// orangeBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "orange";
+// });
+// yellowBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "yellow";
+// });
+// greenBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "green";
+// });
+// blueBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "blue";
+// });
+// purpleBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "purple";
+// });
+// pinkBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "pink";
+// });
+// whiteBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "white";
+// });
+// blackBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "black";
+// });
+// clearBtn.addEventListener("click", function (event : MouseEvent){
+//     color = "white";
+//     for (const p of pairsList){
+//         makeGrid(p[0] - 38, p[1] - 38, p[0] - 1, p[1] - 1, 1);
+//     }
+// });
