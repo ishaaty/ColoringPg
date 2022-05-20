85dd92c354e7633c169a7bb11b75c244a7ea9324
ishaaty <ishatyagi888@gmail.com>
2022-05-20 10:15:40 -0400

    renamed file

---
+++ pixelAlgorithm.js
@@ -0,0 +1,7 @@
+var Pixels = /** @class */ (function () {
+    function Pixels(xCoordinate, yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+    return Pixels;
+}());
