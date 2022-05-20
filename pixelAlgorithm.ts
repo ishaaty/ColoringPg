8dffbc8523cdde26bc08a59c34aa33c1d89d8f3c
ishaaty <ishatyagi888@gmail.com>
2022-05-20 10:15:49 -0400

    ?

---
+++ pixelAlgorithm.ts
@@ -0,0 +1,11 @@
+
+class Pixels {
+    xCoordinate : number;
+    yCoordinate : number;
+
+    constructor (xCoordinate : number, yCoordinate : number){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+}
