0b20bb0e612066cc38b8cc67d229fd8be7329c32
Julia Thompson <jewelst2014@gmail.com>
2022-05-20 10:14:40 -0400

    Worked on Pixels

--- algorithm.ts
+++ algorithm.ts
@@ -15,4 +15,18 @@ class Pixels {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
     }
+
+    draw() {
+        // add the rest of draw
+        this.fill("white");
+    }
+
+    fill(c : string) {
+        ctx.beginPath();
+        ctx.fillStyle = c;
+        ctx.arc(this.xCoordinate, this.yCoordinate, 1, 0, Math.PI*2);
+        ctx.fill();
+        ctx.closePath();
+    }
+
 }
