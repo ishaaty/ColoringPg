1b41304706bea8bf542e13726fd3db09edfcacb1
ishaaty <ishatyagi888@gmail.com>
2022-05-20 20:12:15 -0400

    created pixel class

--- pixelAlgorithm.ts
+++ pixelAlgorithm.ts
@@ -1,24 +1,62 @@
+let canvas = document.querySelector("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-class Pixels {
+// red : 0
+// orange : 1
+// yellow : 2
+// green : 3
+// blue : 4
+// purple : 5
+// pink : 6
+// brown : 7
+// white : 8
+// black : 9
+
+class Pixel {
     xCoordinate : number;
     yCoordinate : number;
+    color : string;
 
-    constructor (xCoordinate : number, yCoordinate : number){
+    constructor (xCoordinate : number, yCoordinate : number, color? : string){
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
+        this.color = color || "white"
     }
 
-    draw() {
-        // add the rest of draw
-        this.fill("white");
+    changeColor(color : string){
+        this.color = color
     }
 
-    fill(c : string) {
+    fill(x : number = 0, y : number = 0) : void {
         ctx.beginPath();
-        ctx.fillStyle = c;
-        ctx.arc(this.xCoordinate, this.yCoordinate, 1, 0, Math.PI*2);
+        ctx.fillStyle = this.color;
+        ctx.arc(x, y, 1, 0, Math.PI*2);
         ctx.fill();
         ctx.closePath();
     }
 
+    makeOrColorGrid(xMin : number, yMin : number, xMax : number, yMax : number, inc : number) {
+        // making vertical lines
+        for (let xUpdate = xMin; xUpdate < xMax; xUpdate += inc){
+            for (let yUpdate = yMin; yUpdate < yMax; yUpdate++){
+                this.fill(xUpdate, yUpdate);
+            }
+        }
+
+        // making horizontal lines
+        for (let yUpdate = yMin; yUpdate < yMax; yUpdate += inc){ 
+            for (let xUpdate = xMin; xUpdate < xMax; xUpdate++){
+                this.fill(xUpdate, yUpdate);
+            }
+        }
+    }
+    
 }
+
+
+// add number sub-class
+// ctx.font = "15px Calibri";
+// ctx.fillStyle = "black";
+// ctx.fillText("1", 17, 25);
+
+
