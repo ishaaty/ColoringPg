4450f4917c3c3feaf3d5cd320b2391ed51154917
ishaaty <ishatyagi888@gmail.com>
2022-05-20 20:11:49 -0400

    created eventHandler.ts

---
+++ eventHandler.ts
@@ -0,0 +1,48 @@
+let color = "white";
+let pixelsList = [];
+
+// filling pairsList
+for(let x = 0; x <= canvas.height; x += 40){
+    for (let y = 0; y <= canvas.width; y += 40){
+        let pixel : Pixel = new Pixel(x, y, color);
+        pixelsList.push(pixel);
+    }
+}
+
+// console.log(pixelsList);
+
+window.addEventListener("load", function () {
+    // creating the grid
+    let pixelGrid : Pixel = pixelsList[0]; // contains coordinates (0,0)
+    pixelGrid.changeColor("black");
+    pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
+
+    // filling the grid with white squares
+    for (const p of pixelsList){
+        p.changeColor("white");
+        p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+        // p.changeColor("black");
+    }
+});
+
+// changing color
+document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
+    elm.addEventListener("click", function (event : MouseEvent){
+        let clickedColor = event.currentTarget as HTMLElement;
+        for (const p of pixelsList){
+            p.changeColor(clickedColor.dataset.key);
+            console.log(`Changed color to ${clickedColor.dataset.key} !`)
+        }
+    })
+});
+
+// to color in pixel when user clicks
+canvas.addEventListener("click", function (event : MouseEvent){
+    for (const p of pixelsList){
+        if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
+            p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+            console.log("Clicked canvas!")
+        }
+    }
+});
+
