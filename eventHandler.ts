9f7c2a37b3b7fb4fda75a502475ef8367ff711d3
ishaaty <ishatyagi888@gmail.com>
2022-05-24 12:01:02 -0400

    commented out

--- eventHandler.ts
+++ eventHandler.ts
@@ -9,6 +9,21 @@ for(let x = 0; x <= canvas.height; x += 40){
     }
 }
 
+// window.addEventListener("load", function () {
+//     // creating the grid
+//     let pixelGrid : Pixel = pixelsList[0]; // contains coordinates (0,0)
+//     pixelGrid.changeColor("black");
+//     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
+
+//     // filling the grid with white squares
+//     for (const p of pixelsList){
+//         p.changeColor("white");
+//         p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+//         p.changeColor("black");
+//     }
+// });
+
+
 window.addEventListener("load", function () {
     // creating the grid
     let pixelGrid : Pixel = pixelsList[0]; // contains coordinates (0,0)
@@ -16,12 +31,11 @@ window.addEventListener("load", function () {
     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
 
     // filling the grid with white squares
-    for (const p of pixelsList){
-        p.changeColor("white");
-        p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
-        p.changeColor("black");
+    for (const r of rainbowHeartArray){
+        r.insertNum(r.xCoordinate, r.yCoordinate, r.num);
     }
 });
+
 
 // changing color
 document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
@@ -37,6 +51,7 @@ document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
 canvas.addEventListener("click", function (event : MouseEvent){
     for (const p of pixelsList){
         if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
+            console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
             p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
         }
     }
