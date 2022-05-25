b9e40e7f7286ec709e546ac63a6c949b31c33bbb
ishaaty <ishatyagi888@gmail.com>
2022-05-25 11:26:15 -0400

    js file

--- eventHandler.js
+++ eventHandler.js
@@ -1,11 +1,10 @@
-"use strict";
-let color = "black";
-let pixelsList = [];
-let currentColor = document.querySelector("#currentColor");
+var color = "black";
+var pixelsList = [];
+var currentColor = document.querySelector("#currentColor");
 // filling pairsList
-for (let x = 0; x <= canvas.height; x += 40) {
-    for (let y = 0; y <= canvas.width; y += 40) {
-        let pixel = new Pixel(x, y, color);
+for (var x = 0; x <= canvas.height; x += 40) {
+    for (var y = 0; y <= canvas.width; y += 40) {
+        var pixel = new Pixel(x, y, color);
         pixelsList.push(pixel);
     }
 }
@@ -23,29 +22,41 @@ for (let x = 0; x <= canvas.height; x += 40) {
 // });
 window.addEventListener("load", function () {
     // creating the grid
-    let pixelGrid = pixelsList[0]; // contains coordinates (0,0)
+    var pixelGrid = pixelsList[0]; // contains coordinates (0,0)
     pixelGrid.changeColor("black");
     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
     // filling the grid with white squares
-    for (const r of rainbowHeartArray) {
+    for (var _i = 0, rainbowHeartArray_1 = rainbowHeartArray; _i < rainbowHeartArray_1.length; _i++) {
+        var r = rainbowHeartArray_1[_i];
         r.insertNum(r.xCoordinate, r.yCoordinate, r.num);
     }
 });
 // changing color
 document.querySelectorAll(".colorBtn").forEach(function (elm) {
     elm.addEventListener("click", function (event) {
-        let clickedColor = event.currentTarget;
-        currentColor.textContent = (`Current Color: ${clickedColor.textContent}`);
-        for (const p of rainbowHeartArray) {
+        var clickedColor = event.currentTarget;
+        currentColor.textContent = ("Current Color: ".concat(clickedColor.textContent));
+        for (var _i = 0, rainbowHeartArray_2 = rainbowHeartArray; _i < rainbowHeartArray_2.length; _i++) {
+            var p = rainbowHeartArray_2[_i];
             p.changeColor(clickedColor.dataset.key);
         }
     });
 });
+// // coloring in a pixel when user clicks
+// canvas.addEventListener("click", function (event : MouseEvent){
+//     for (const p of pixelsList){
+//         if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
+//             console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
+//             p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+//         }
+//     }
+// });
 // coloring in a pixel when user clicks
 canvas.addEventListener("click", function (event) {
-    for (const p of rainbowHeartArray) {
+    for (var _i = 0, rainbowHeartArray_3 = rainbowHeartArray; _i < rainbowHeartArray_3.length; _i++) {
+        var p = rainbowHeartArray_3[_i];
         if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)) {
-            console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
+            console.log("".concat(p.color, ": (").concat(p.xCoordinate, ", ").concat(p.yCoordinate, ")"));
             p.checkColor(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
         }
     }
