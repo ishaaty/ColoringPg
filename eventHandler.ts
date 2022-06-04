b32e9454a7c0590cc7d2b5459dc93eacf2f1e074
ishaaty <ishatyagi888@gmail.com>
2022-06-04 11:34:06 -0400

    added bol and comments

--- eventHandler.ts
+++ eventHandler.ts
@@ -1,9 +1,12 @@
 let currentColor = document.querySelector("#currentColor") as HTMLParagraphElement;
 let clearBtn = document.querySelector("#clear") as HTMLButtonElement;
 let cor = sessionStorage.getItem("first") as string;
-let color = "black";
+let color : string = "black";
+let bol : boolean = true;
 let pixelsList = [];
 let selectedArray = [];
+
+// sets selected array
 if (cor === "freeplay") {
     selectedArray = freeplayArray;
 }
@@ -11,7 +14,7 @@ if (cor === "rainbowHeart") {
     selectedArray = rainbowHeartArray;
 }
 
-// filling pairsList
+// filling pairsList for freeplay screen
 for(let x = 0; x <= canvas.height; x += 40){
     for (let y = 0; y <= canvas.width; y += 40){
         let pixel : Pixel = new Pixel(x, y, color);
@@ -19,36 +22,19 @@ for(let x = 0; x <= canvas.height; x += 40){
     }
 }
 
-// window.addEventListener("load", function () {
-//     // creating the grid
-//     let pixelGrid : Pixel = pixelsList[0]; // contains coordinates (0,0)
-//     pixelGrid.changeColor("black");
-//     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
-
-//     // filling the grid with white squares
-//     for (const p of pixelsList){
-//         p.changeColor("white");
-//         p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
-//         p.changeColor("black");
-//     }
-// });
-
-
 window.addEventListener("load", function () {
     // creating the grid
     let pixelGrid : Pixel = pixelsList[0]; // contains coordinates (0,0)
     pixelGrid.changeColor("black");
     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
 
-    // filling the grid with white squares
+    // filling the grid with numbers
     for (const r of selectedArray){
         if (r.constructor.name === "CheckablePixel") {
             r.insertNum(r.xCoordinate, r.yCoordinate, r.num);
         }
     }
-
 });
-
 
 // changing color
 document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
@@ -62,15 +48,16 @@ document.querySelectorAll<HTMLElement>(".colorBtn").forEach(function(elm){
 });
 
 // coloring in a pixel when user clicks
-canvas.addEventListener("click", function (event : MouseEvent){
-    for (const p of selectedArray){
-        if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
-            console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
-            p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+canvas.addEventListener("click", function(event : MouseEvent){
+    canvas.addEventListener("mousedown", function (event : MouseEvent){
+        for (const p of selectedArray){
+            if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
+                console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
+                p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+            }
         }
-    }
+    })
 });
-// });
 
 clearBtn.addEventListener("click", function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -79,7 +66,7 @@ clearBtn.addEventListener("click", function() {
     pixelGrid.changeColor("black");
     pixelGrid.makeOrColorGrid(0, 0, canvas.width, canvas.height, 40);
 
-    // filling the grid with white squares
+    // re-inserts num into grid
     for (const r of selectedArray){
         if (r.constructor.name === "CheckablePixel") {
             r.insertNum(r.xCoordinate, r.yCoordinate, r.num);
