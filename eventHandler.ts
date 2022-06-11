8e68e378b1d6b2381607f9259c66592c7b2dff17
ishaaty <ishatyagi888@gmail.com>
2022-06-11 10:47:20 -0400

    fixed color issue

--- eventHandler.ts
+++ eventHandler.ts
@@ -60,7 +60,7 @@ function colorPixel(event : MouseEvent){
     for (const p of selectedArray){
         if ((event.offsetX <= p.xCoordinate && event.offsetX >= p.xCoordinate - 40) && (event.offsetY <= p.yCoordinate && event.offsetY >= p.yCoordinate - 40)){
                 console.log(`${p.color}: (${p.xCoordinate}, ${p.yCoordinate})`);
-            p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
+                p.makeOrColorGrid(p.xCoordinate - 38, p.yCoordinate - 38, p.xCoordinate - 1, p.yCoordinate - 1, 1);
         }
     }
 }
